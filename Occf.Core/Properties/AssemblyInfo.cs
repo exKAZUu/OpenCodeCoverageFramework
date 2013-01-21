df0a05e71bdcb0e2257743101593a4dee46700b0
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-21 19:54:35 +0900

    Fix dependency to use invalid Code2Xml.

--- Occf.Core/Properties/AssemblyInfo.cs
+++ Occf.Core/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.84")]
-[assembly: AssemblyFileVersion("1.1.0.84")]
+[assembly: AssemblyVersion("1.1.0.87")]
+[assembly: AssemblyFileVersion("1.1.0.87")]
