42da55409d7c56d4258416981e8c7789290c3eec
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-12-10 19:09:43 +0900

    Adding grouping algorithm.

--- Occf.Learner.Tests/ClassificationTest.cs
+++ Occf.Learner.Tests/ClassificationTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using NUnit.Framework;
+using Paraiba.Linq;
 
 namespace Occf.Learner.Core.Tests {
 	[TestFixture]
@@ -10,32 +11,44 @@ namespace Occf.Learner.Core.Tests {
 		[Test]
 		public void Classify() {
 			var accepted = new List<BigInteger> {
-				(1 << 4) + (1 << 3) + (1 << 2) + (0 << 2) + (1 << 0),
-				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 2) + (1 << 0),
-				(1 << 4) + (1 << 3) + (0 << 2) + (0 << 2) + (1 << 0),
-				(0 << 4) + (1 << 3) + (1 << 2) + (1 << 2) + (0 << 0),
-				(1 << 4) + (1 << 3) + (1 << 2) + (1 << 2) + (0 << 0)
+				(1 << 4) + (1 << 3) + (1 << 2) + (0 << 1) + (1 << 0),
+				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 1) + (1 << 0),
+				(1 << 4) + (1 << 3) + (0 << 2) + (0 << 1) + (1 << 0),
+				(0 << 4) + (1 << 3) + (1 << 2) + (1 << 1) + (0 << 0),
+				(1 << 4) + (1 << 3) + (1 << 2) + (1 << 1) + (0 << 0)
 			};
 			var seeds = new List<BigInteger> {
-				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 2) + (1 << 0),
-				(1 << 4) + (0 << 3) + (1 << 2) + (1 << 2) + (1 << 0),
+				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 1) + (1 << 0),
+				(1 << 4) + (0 << 3) + (1 << 2) + (1 << 1) + (1 << 0),
 			};
 			var rejected = new List<BigInteger> {
-				(0 << 4) + (1 << 3) + (1 << 2) + (0 << 2) + (1 << 0),
-				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 2) + (0 << 0),
-				(1 << 4) + (1 << 3) + (1 << 2) + (0 << 2) + (0 << 0),
-				(0 << 4) + (1 << 3) + (0 << 2) + (1 << 2) + (1 << 0)
+				(0 << 4) + (1 << 3) + (1 << 2) + (0 << 1) + (1 << 0),
+				(1 << 4) + (0 << 3) + (0 << 2) + (1 << 1) + (0 << 0),
+				(1 << 4) + (1 << 3) + (1 << 2) + (0 << 1) + (0 << 0),
+				(0 << 4) + (1 << 3) + (0 << 2) + (1 << 1) + (1 << 0)
 			};
-			var learningSet = seeds.Select(s => new List<BigInteger> { s }).ToList();
+			var learningSet = seeds
+					.Select(s => new Stack<BigInteger>(Enumerable.Repeat(s, 1)))
+					.ToList();
 
+			var histories = new Stack<int>();
 			for (int i = 0; i < accepted.Count; i++) {
-				var predicates = learningSet.Select(GetPredicate).ToList();
-				for (int j = 0; j < learningSet.Count; j++) {
-					learningSet[j].Add(accepted[i]);
-					if (CanClassify(predicates, rejected)) {
+				for (int j = 0;; j++) {
+					learningSet[j].Push(accepted[i]);
+					var predicates = learningSet.Select(GetPredicate).ToList();
+					if (CanReject(predicates, rejected)) {
+						histories.Push(j);
 						break;
 					}
-					learningSet[j].RemoveAt(learningSet[j].Count - 1);
+					learningSet[j].Pop();
+					while (j == learningSet.Count - 1) {
+						if (histories.IsEmpty()) {
+							throw new Exception("Failed to learn");
+						}
+						i--;
+						j = histories.Pop();
+						learningSet[j].Pop();
+					}
 				}
 			}
 
@@ -51,8 +64,8 @@ namespace Occf.Learner.Core.Tests {
 			return properties.Aggregate((p1, p2) => p1 & p2);
 		}
 
-		private bool CanClassify(IEnumerable<BigInteger> predicates, IEnumerable<BigInteger> rejected) {
-			return predicates.All(p => rejected.All(r => (p & r) == p));
+		private bool CanReject(IEnumerable<BigInteger> predicates, IEnumerable<BigInteger> rejected) {
+			return predicates.All(p => rejected.All(r => (p & r) != p));
 		}
 	}
 }
